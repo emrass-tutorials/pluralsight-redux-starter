0edd43a85dd5300a13b0531f41e385a68cff34a5
Enrico Mraß <enrico.mrass@sap.com>
2016-10-23 14:11:37 +0200

    Redux Flow: mapDispatchToProps Manual Mapping

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -44,7 +44,7 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  createCourse: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired
 };
 
@@ -55,5 +55,10 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-// react will inject a dispatch method into this.props in the component since we do not specify the mapDispatchToProps function
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispathToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(CoursesPage);
