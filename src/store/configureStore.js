441264b333791d85930c532bbbc2c22f5895d8e3
Enrico Mraß <enrico.mrass@sap.com>
2016-10-23 16:55:43 +0200

    Async in Redux: Add Thunk to Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 // Be sure to ONLY add redux-immutable-state-invariant in development!
 const middleware = process.env.NODE_ENV !== 'production' ?
-  [reduxImmutableStateInvariant()] :
-  [];
+  [reduxImmutableStateInvariant(), thunk] :
+  [thunk];
 
 export default function configureStore(initialState) {
   return createStore(
