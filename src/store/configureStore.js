f530b3793b1bd0067baf7fa2b3cd89d97df6dac9
Enrico Mraß <enrico.mrass@sap.com>
2016-10-23 12:50:09 +0200

    Redux Flow: Store

---
+++ src/store/configureStore.js
@@ -0,0 +1,12 @@
+// configure the store when the app starts up (entry function)
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}
