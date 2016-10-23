90b5fc60f25cba803932b7473727d7a73c22f5a9
Enrico Mraß <enrico.mrass@sap.com>
2016-10-23 17:20:40 +0200

    Async in Redux: Dispatch Action on Page Load

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // create an instance of the store
 // we could pass initial state to the store, but this is optional and mostly useful for server-side rendering
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
