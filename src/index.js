13aa8f5d43c02cbb77caf16631eaafc5e81abaeb
Enrico Mraß <enrico.mrass@sap.com>
2016-10-23 13:16:27 +0200

    Redux Flow: Instantiate Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+// create an instance of the store
+// we could pass initial state to the store, but this is optional and mostly useful for server-side rendering
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
