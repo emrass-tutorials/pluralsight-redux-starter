4deeaa62eae68024c6d33e907f40764de027521e
Enrico Mraß <enrico.mrass@sap.com>
2016-10-29 13:52:41 +0200

    Async Writes in Redux: Create Form Input Components - correct the SelectInput default option

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -10,6 +10,7 @@ const SelectInput = ({name, label, onChange, defaultOption, value, error, option
           value={value}
           onChange={onChange}
           className="form-control">
+          <option value="">{defaultOption}</option>
           {options.map((option) => {
             return <option key={option.value} value={option.value}>{option.text}</option>;
           })}
